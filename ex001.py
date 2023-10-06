b79fad183b9432cf7692f4c640cf7d567c05d14c
rodrigo_souza07@outlook.com <you@example.com>
2023-10-05 21:43:40 -0400

    Inciando com dash

---
+++ ex001.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import time
+import plotly.express as px
+import streamlit as st
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+
+adidas = pd.read_excel("Adidas.xlsx")
+adidas.head(5)
+
+
+
