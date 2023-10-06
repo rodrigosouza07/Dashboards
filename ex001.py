2f3d2e86bc90a32db08210b5ab2c366b2e0de1b7
rodrigo_souza07@outlook.com <you@example.com>
2023-10-05 21:45:43 -0400

    mudanca de base dados

--- ex001.py
+++ ex001.py
@@ -1,11 +1,24 @@
-import pandas as pd
-import numpy as np
-import time
+from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
-import streamlit as st
-import dash_bootstrap_components as dbc
-import dash_core_components as dcc
+import pandas as pd
 
-adidas = pd.read_excel("Adidas.xlsx")
-adidas.head(5)
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
 
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
+    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    dcc.Graph(id='graph-content')
+])
+
+@callback(
+    Output('graph-content', 'figure'),
+    Input('dropdown-selection', 'value')
+)
+def update_graph(value):
+    dff = df[df.country==value]
+    return px.line(dff, x='year', y='pop')
+
+if __name__ == '__main__':
+    app.run(debug=True)
